c04cee8e7f56c4b61cb7271b95f8a05bf17b07a4
Paige Mundy <paigemundy@Paiges-MBP.lan>
2023-02-02 16:55:50 -0600

    first commit

---
+++ controllers/stripeController.js
@@ -0,0 +1,17 @@
+const stripe = require('stripe')(process.env.STRIPE_KEY);
+
+const stripeController = async (req, res) => {
+const {purchase, total_amount, shipping_fee} = req.body;
+const calculatOrderAmount = () => {
+  return total_amount + shipping_fee
+}
+const paymentIntent = await stripe.paymentIntent.create({
+  amount: calculatOrderAmount,
+  currency: 'usd',
+})
+res.json({clientSecret:paymentIntent.client_secret})
+
+  res.send('stripe route');
+};
+
+module.exports = stripeController;
