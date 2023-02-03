aa362f2b93e705834fca500b660de00c055003c5
Paige Mundy <paigemundy@Paiges-MBP.lan>
2023-02-03 10:36:19 -0600

    second commit

--- controllers/stripeController.js
+++ controllers/stripeController.js
@@ -5,13 +5,13 @@ const {purchase, total_amount, shipping_fee} = req.body;
 const calculatOrderAmount = () => {
   return total_amount + shipping_fee
 }
-const paymentIntent = await stripe.paymentIntent.create({
-  amount: calculatOrderAmount,
+const paymentIntent = await stripe.paymentIntents.create({
+  amount: calculatOrderAmount(),
   currency: 'usd',
 })
 res.json({clientSecret:paymentIntent.client_secret})
 
-  res.send('stripe route');
+//  res.send('stripe route');
 };
 
 module.exports = stripeController;
